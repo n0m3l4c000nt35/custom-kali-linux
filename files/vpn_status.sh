7378ce7c6d765f0ce4061ef512810dcac9840061
Esteban Zárate <zar23001@byui.edu>
2025-05-31 15:38:55 -0300

    Update vpn_status.sh

--- files/vpn_status.sh
+++ files/vpn_status.sh
@@ -3,7 +3,7 @@
 IFACE=$(ip -o link show | awk -F': ' '/tun0/ {print $2}')
 
 if [ "$IFACE" = "tun0" ]; then
-  echo "%{T2}%{F#1bbf3e}<U+F01A7>%{T-} %{F#fff}$(ip a show tun0 | grep -oP '(?<=inet\s)\d+\.\d+\.\d+\.\d+')"
+  echo "%{T2}%{F#1bbf3e}󰆧%{T-} %{F#fff}$(ip a show tun0 | grep -oP '(?<=inet\s)\d+\.\d+\.\d+\.\d+')"
 else
   echo ""
 fi
