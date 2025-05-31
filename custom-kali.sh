1a724e30505301b7efeb2af62d8a447965afbddb
Esteban Zárate <zar23001@byui.edu>
2025-05-31 13:51:41 -0300

    Create custom-kali.sh

---
+++ custom-kali.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+sudo apt install -y virtualbox-guest-x11 linux-headers-$(uname-r) xorg dkms build-essential bspwm kitty feh polybar i3lock xclip firefox-esr ntpsec-ntpdate locate
+sudo updatedb
+
+mkdir -p $HOME/.config/{bspwm,sxhkd,kitty,polybar}
+mkdir $HOME/.config/bspwm/scripts
+mkdir $HOME/.config/polybar/scripts
+sudo mkdir /opt/nvim
+sudo mkdir /usr/share/fonts/truetype/hacknerd
+sudo mkdir /usr/share/zsh-sudo
+
+wget -P $HOME https://raw.githubusercontent.com/n0m3l4c000nt35/custom-kali-linux/refs/heads/main/files/.xinitrc
+wget -P $HOME https://raw.githubusercontent.com/n0m3l4c000nt35/custom-kali-linux/refs/heads/main/files/.Xresources
+wget -P $HOME/.config/bspwm/ https://raw.githubusercontent.com/n0m3l4c000nt35/custom-kali-linux/refs/heads/main/files/bspwmrc
+wget -P $HOME/.config/sxhkd/ https://raw.githubusercontent.com/n0m3l4c000nt35/custom-kali-linux/refs/heads/main/files/sxhkdrc
+wget -P $HOME/.config/polybar/scripts/ https://raw.githubusercontent.com/n0m3l4c000nt35/custom-kali-linux/refs/heads/main/files/ethernet_status.sh
+wget -P $HOME/.config/polybar/scripts/ https://raw.githubusercontent.com/n0m3l4c000nt35/custom-kali-linux/refs/heads/main/files/vpn_status.sh
+wget -P $HOME/.config/polybar/scripts/ https://raw.githubusercontent.com/n0m3l4c000nt35/custom-kali-linux/refs/heads/main/files/target_to_hack.sh
+wget -P $HOME/.config/polybar/scripts/ https://raw.githubusercontent.com/n0m3l4c000nt35/custom-kali-linux/refs/heads/main/files/copy_target.sh
+wget -P $HOME/.config/bspwm/scripts/ https://raw.githubusercontent.com/n0m3l4c000nt35/custom-kali-linux/refs/heads/main/files/bspwm_resize
+wget -P $HOME/.config/polybar/ https://raw.githubusercontent.com/n0m3l4c000nt35/custom-kali-linux/refs/heads/main/files/launch.sh
+wget -P $HOME/.config/polybar/ https://raw.githubusercontent.com/n0m3l4c000nt35/custom-kali-linux/refs/heads/main/files/config.ini
+
+touch $HOME/.config/polybar/scripts/target.txt
+
+chmod u+x $HOME/.config/bspwm/bspwmrc
+chmod +x $HOME/.config/polybar/scripts/{ethernet_status.sh,vpn_status.sh,target_to_hack.sh,copy_target.sh}
+chmod +x $HOME/.config/bspwm/scripts/bspwm_resize
+chmod +x $HOME/.config/polybar/launch.sh
+
+sudo wget -P /usr/share/fonts/truetype/hacknerd https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Hack.zip
+sudo unzip /usr/share/fonts/truetype/hacknerd/Hack.zip -d /usr/share/fonts/truetype/hacknerd
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+
+sudo wget -P /usr/share/zsh-sudo/ https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/refs/heads/master/plugins/sudo/sudo.plugin.zsh
+
+wget -P $HOME/Downloads https://github.com/sharkdp/bat/releases/download/v0.25.0/bat_0.25.0_amd64.deb
+wget -P $HOME/Downloads https://github.com/lsd-rs/lsd/releases/download/v1.1.5/lsd_1.1.5_amd64.deb
+sudo dpkg -i $HOME/Downloads/bat_0.25.0_amd64.deb
+sudo dpkg -i $HOME/Downloads/lsd_1.1.5_amd64.deb
+rm $HOME/Downloads/bat_0.25.0_amd64.deb $HOME/Downloads/lsd_1.1.5_amd64.deb
+
+git clone https://github.com/NvChad/starter $HOME/.config/nvim
+wget -P $HOME/Downloads https://github.com/neovim/neovim/releases/download/v0.11.0/nvim-linux-x86_64.tar.gz
+sudo mv $HOME/Downloads/nvim-linux-x86_64.tar.gz /opt/nvim
+sudo tar -xf /opt/nvim/nvim-linux-x86_64.tar.gz -C /opt/nvim
+sudo rm /opt/nvim/nvim-linux-x86_64.tar.gz
+sudo ln -s /opt/nvim/nvim-linux-x86_64/bin/nvim /usr/bin/nvim
+
+git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf && $HOME/.fzf/install
+
+sudo git clone https://github.com/meskarune/i3lock-fancy.git /opt/i3lock-fancy
+sudo make -C /opt/i3lock-fancy install
+
+xdg-settings set default-web-browser firefox-esr.desktop
