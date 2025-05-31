e355583ca74ff023dd26f0fc17e970872193aa0a
Esteban Zárate <zar23001@byui.edu>
2025-05-31 13:27:50 -0300

    Create ethernet_status.sh

---
+++ files/ethernet_status.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ETH=$(ip -4 a show <interface> | grep -oP '(?<=inet\s)\d+\.\d+\.\d+\.\d+')
+
+if [ -n "$ETH" ]; then
+  echo "%{T2}%{F#2494e7}<U+F0200>%{T-} %{F#fff}$(ip -4 a show <interface> | grep -oP '(?<=inet\s)\d+\.\d+\.\d+\.\d+')"
+else
+  echo "%{T2}%{F#808080}<U+F0200>%{T-} %{F#fff} Ups!"
+fi
