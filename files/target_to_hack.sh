0b39dfe6812e935f42ee4f02584f08e3f225bbc8
Esteban Zárate <zar23001@byui.edu>
2025-05-31 15:38:13 -0300

    Update target_to_hack.sh

--- files/target_to_hack.sh
+++ files/target_to_hack.sh
@@ -3,7 +3,7 @@
 ip_address=$(/bin/cat $HOME/.config/polybar/scripts/target.txt)
 
 if [ -n "$ip_address" ]; then
-  echo "%{T2}%{F#ff0000}<U+F04FE>%{T-} %{F#fff}$ip_address"
+  echo "%{T2}%{F#ff0000}󰓾%{T-} %{F#fff}$ip_address"
 else
   echo ""
 fi
