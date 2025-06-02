ebb80c8355e9e9ed123a79cd7a849d343a053891
Esteban Zárate <zar23001@byui.edu>
2025-06-02 15:36:34 -0300

    Update custom-kali.sh

--- custom-kali.sh
+++ custom-kali.sh
@@ -56,3 +56,5 @@ git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf && $HOME/.fzf
 
 sudo git clone https://github.com/meskarune/i3lock-fancy.git /opt/i3lock-fancy
 sudo make -C /opt/i3lock-fancy install
+
+startx
